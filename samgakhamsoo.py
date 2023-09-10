6230c098e57a6fbd1ccb812dc2d9d5078cfe92c3
Lee Yeonwoo <ckadltmf0224@naver.com>
2023-09-11 07:45:07 +0900

    09111

---
+++ samgakhamsoo.py
@@ -0,0 +1,14 @@
+import math
+
+start = (1, 1)
+end = (3, 3)
+goal = (5, 7)
+
+y = abs(goal[0] - start[0])
+x = abs(goal[1] - start[1])
+
+a = math.sqrt(y ** 2 + x ** 2)
+
+ga = math.atan(y / x)
+
+print(a, math.degrees(ga))
