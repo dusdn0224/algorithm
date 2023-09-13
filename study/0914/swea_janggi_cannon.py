67e51cc41af5b66c3ebf6c27981df9987e4947d5
이연우 <ckadltmf0224@naver.com>
2023-09-13 17:37:24 +0900

    0913

--- study/0914/swea_janggi_cannon.py
+++ study/0914/swea_janggi_cannon.py
@@ -1,3 +1,88 @@
+def game(i, j, move, lst):
+    if move == 3:
+        return
+    start_y, start_x = i, j
+    two = 0
+    while 0 <= i - 1:
+        i -= 1
+        if arr[i][j] == 1 and (i, j) not in lst:
+            two += 1
+            while 0 <= i - 1:
+                i -= 1
+                if arr[i][j] == 1 and (i, j) not in lst:
+                    two += 1
+                    lst.append((i, j))
+                    visited[i][j] = 1
+                    game(i, j, move + 1, lst)
+                    lst.pop()
+                    if two == 2:
+                        break
+                else:
+                    two = 0
+                    game(i, j, move + 1, lst)
+            break
+    i, j = start_y, start_x
+    two = 0
+    while i + 1 < N:
+        i += 1
+        if arr[i][j] == 1 and (i, j) not in lst:
+            two += 1
+            while i + 1 < N:
+                i += 1
+                if arr[i][j] == 1 and (i, j) not in lst:
+                    two += 1
+                    lst.append((i, j))
+                    visited[i][j] = 1
+                    game(i, j, move + 1, lst)
+                    lst.pop()
+                    if two == 2:
+                        break
+                else:
+                    two = 0
+                    game(i, j, move + 1, lst)
+            break
+    i, j = start_y, start_x
+    two = 0
+    while 0 <= j - 1:
+        j -= 1
+        if arr[i][j] == 1 and (i, j) not in lst:
+            two += 1
+            while 0 <= j - 1:
+                j -= 1
+                if arr[i][j] == 1 and (i, j) not in lst:
+                    two += 1
+                    lst.append((i, j))
+                    visited[i][j] = 1
+                    game(i, j, move + 1, lst)
+                    lst.pop()
+                    if two == 2:
+                        break
+                else:
+                    two = 0
+                    game(i, j, move + 1, lst)
+            break
+    i, j = start_y, start_x
+    two = 0
+    while j + 1 < N:
+        j += 1
+        if arr[i][j] == 1 and (i, j) not in lst:
+            two += 1
+            while j + 1 < N:
+                j += 1
+                if arr[i][j] == 1 and (i, j) not in lst:
+                    two += 1
+                    lst.append((i, j))
+                    visited[i][j] = 1
+                    game(i, j, move + 1, lst)
+                    lst.pop()
+                    if two == 2:
+                        break
+                else:
+                    two = 0
+                    game(i, j, move + 1, lst)
+            break
+
+
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
@@ -5,8 +90,17 @@ for tc in range(1, T+1):
     sy = sx = 0
     for n1 in range(N):
         lst = list(map(int, input().split()))
-        arr.append(lst)
         for n2 in range(N):
             if lst[n2] == 2:
                 sy, sx = n1, n2
-    print(arr, sy, sx)
+                lst[n2] = 0
+        arr.append(lst)
+    cnt = 0
+    visited = [[0] * N for _ in range(N)]
+    game(sy, sx, 0, [])
+    for y in range(N):
+        for x in range(N):
+            if visited[y][x]:
+                cnt += 1
+    print(visited)
+    print(f'#{tc}', cnt)
