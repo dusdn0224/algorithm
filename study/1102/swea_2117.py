84150051a4e27a04e297aaea3b07f79e2ea9ea71
이연우 <ckadltmf0224@naver.com>
2023-11-02 09:21:20 +0900

    1102jooseok

--- study/1102/swea_2117.py
+++ study/1102/swea_2117.py
@@ -6,6 +6,7 @@ def service(n):
             area.append((i, j))  # 오른쪽 끝
             area.append((i, -j))  # 왼쪽 끝
     return list(set(area))
+    # return 값은 [(0, 0), (-1, 0), (1, 0), (0, 1), (0, -1)] 이런식
 
 
 T = int(input())
@@ -13,16 +14,17 @@ for tc in range(1, T+1):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
     max_cnt = 0
-    for k in range(1, N + 1 - (N % 2) + 1):
+    for k in range(1, N + 1 - (N % 2) + 1):  # 범위는 넉넉하게 잡기
         direct = service(k)
         for p in range(N):
             for q in range(N):
                 cnt = 0
-                for di, dj in direct:
+                for di, dj in direct:  # 마름모 모양 확인하기
                     ni, nj = p + di, q + dj
-                    if 0 <= ni < N and 0 <= nj < N:
-                        if arr[ni][nj]:
+                    if 0 <= ni < N and 0 <= nj < N:  # 도시 안에 있고
+                        if arr[ni][nj]:  # 집이면
                             cnt += 1
+                # 손해만 안보면 되므로 0 이상
                 if (cnt * M) - len(direct) >= 0:
                     if max_cnt < cnt:
                         max_cnt = cnt
